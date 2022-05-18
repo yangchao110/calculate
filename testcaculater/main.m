c3722dfdde11dd72d984d9ad9fcdf26be816342a
chaoyang <chaoyang@tujia.com>
2022-05-18 15:52:36 +0800

    Initial Commit

---
+++ testcaculater/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testcaculater
+//
+//  Created by tujia on 2022/5/18.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
